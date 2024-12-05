03c99365b05c6eacb3663f4771ed956bb2cc5bf4
Ezekiel Osunbor <zikichips100@gmail.com>
2024-12-05 17:36:43 +0000

    bug fixes

--- src/main/java/com/jemo/RestaurantReviewPortal/awsconfig/AwsS3Config.java
+++ src/main/java/com/jemo/RestaurantReviewPortal/awsconfig/AwsS3Config.java
@@ -1,6 +1,5 @@
 package com.jemo.RestaurantReviewPortal.awsconfig;
 
-import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
@@ -10,32 +9,44 @@ import software.amazon.awssdk.services.s3.S3Client;
 
 @Configuration
 public class AwsS3Config {
-    private final Dotenv dotenv;
-
-    public AwsS3Config() {
-        this.dotenv = Dotenv.configure().load();
-    }
+    // for local environment
+//    private final Dotenv dotenv;
+//
+//    public AwsS3Config() {
+//        this.dotenv = Dotenv.configure().load();
+//    }
 
     @Bean
     public S3Client s3Client() {
         AwsBasicCredentials credentials = AwsBasicCredentials.create(
-                System.getenv("AWS_ACCESS_KEY") != null ?
-                        System.getenv("AWS_ACCESS_KEY") : dotenv.get("AWS_ACCESS_KEY"),
-                System.getenv("AWS_SECRET_KEY") != null ?
-                        System.getenv("AWS_SECRET_KEY") : dotenv.get("AWS_SECRET_KEY")
+                // for production environment
+                System.getenv("AWS_ACCESS_KEY"),
+                System.getenv("AWS_SECRET_KEY")
+
+                // for local environment
 //                dotenv.get("AWS_ACCESS_KEY"),
 //                dotenv.get("AWS_SECRET_KEY")
+
         );
         return S3Client.builder()
-                .region(Region.of(System.getenv("AWS_REGION") != null ?
-                        System.getenv("AWS_REGION") : dotenv.get("AWS_REGION")))
+                .region(
+                        Region.of(
+                                // for production environment
+                                System.getenv("AWS_REGION")
+
+                                // for local environment
+                                // dotenv.get("AWS_REGION"))
+                        ))
                 .credentialsProvider(StaticCredentialsProvider.create(credentials))
                 .build();
     }
 
     @Bean
     public String bucketName() {
-        return System.getenv("AWS_BUCKET_NAME") != null ?
-                System.getenv("AWS_BUCKET_NAME") : dotenv.get("AWS_BUCKET_NAME");
+        // for production environment
+        return System.getenv("AWS_BUCKET_NAME");
+
+        // for local environment
+//        return dotenv.get("AWS_BUCKET_NAME");
     }
 }
